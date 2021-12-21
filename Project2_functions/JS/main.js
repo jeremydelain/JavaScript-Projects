1ff4989e92d72dad790a267e9fff99c5f96641a6
jeremydelain <jeremydelain@gmail.com>
2021-12-21 13:25:43 -0600

    Updated to Step: 71

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function  My_First_Function(){
+    var sentence = "I am learning";                                 //initialize String
+    sentence += " a lot from this program."                         //concatenate String
+    document.getElementById("Concatenate").innerHTML = sentence;    //replace text
+}
