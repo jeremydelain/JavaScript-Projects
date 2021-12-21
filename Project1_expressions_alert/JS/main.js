a7840e978a4a2a0e5cd1c26caa1b15cfa379b2d7
jeremydelain <jeremydelain@gmail.com>
2021-12-21 11:31:24 -0600

    Changed CSS subfolder to JS

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("TEST")                    //Testing alert
+var red = "RED";                        //Defining 3 strings to be colored
+var green = "GREEN";
+var blue = "BLUE";
+document.write(red.fontcolor(red)       //coloring and concatenating strings
+    + " " + green.fontcolor(green) 
+    + " " + blue.fontcolor(blue)); 
+result = 3+3;                           //assigning expression result
+document.write("  3 + 3 =" + result);   //writing result
